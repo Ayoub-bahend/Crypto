eb04f3140f91a488d68a3c7722e71fcc72f5bd74
Ayoub-bahend <ayoubbahend2002@gmail.com>
2024-11-11 16:39:24 +0100

    add table manipulation

--- TP5/common.cpp
+++ TP5/common.cpp
@@ -26,3 +26,36 @@ uint16_t multiplyF216(uint16_t a, uint16_t b) {
     }
     return result;
 }
+
+// Function to generate the logarithm table for a given generator
+std::vector<uint16_t> generateLogTable(uint16_t generator, uint16_t polynomial) {
+    std::vector<uint16_t> logTable(65536, 0);
+    uint16_t value = 1;
+    for (uint16_t i = 0; i < 65535; ++i) {
+        logTable[value] = i;
+        value = multiplyByX(value);
+    }
+    logTable[0] = 65535; // Convention for log(0)
+    return logTable;
+}
+
+// Function to generate the inverse logarithm table
+std::vector<uint16_t> generateInverseLogTable(const std::vector<uint16_t>& logTable) {
+    std::vector<uint16_t> inverseLogTable(65536, 0);
+    for (uint16_t i = 0; i < 65535; ++i) {
+        inverseLogTable[logTable[i]] = i;
+    }
+    inverseLogTable[65535] = 0; // Convention for inverse log(0)
+    return inverseLogTable;
+}
+
+// Function to generate the Zech's logarithm table
+std::vector<uint16_t> generateZechLogTable(const std::vector<uint16_t>& logTable, const std::vector<uint16_t>& inverseLogTable) {
+    std::vector<uint16_t> zechLogTable(65536, 0);
+    for (uint16_t i = 0; i < 65535; ++i) {
+        uint16_t gi = inverseLogTable[i];
+        uint16_t onePlusGi = gi ^ 1;
+        zechLogTable[i] = logTable[onePlusGi];
+    }
+    return zechLogTable;
+}
--- TP5/main.cpp
+++ TP5/main.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
+#include <fstream>
 #include "common.hpp"
 
 void printSeparator() {
     std::cout << "----------------------------------------" << std::endl;
 }
 
+
 int main() {
     printSeparator();
     std::cout << "Mini-Rijndael Arithmetic in F216 Demonstration" << std::endl;
     printSeparator();
-
     uint16_t a = 0x1234;
     uint16_t b = 0x5678;
-
     uint16_t sum = addF216(a, b);
     uint16_t product = multiplyF216(a, b);
-
     std::cout << "Sum of " << std::hex << a << " and " << b << " in F216: " << sum << std::endl;
     std::cout << "Product of " << std::hex << a << " and " << b << " in F216: " << product << std::endl;
+    printSeparator();
+    std::cout << "End of Demonstration" << std::endl;
+    printSeparator();
 
+
+
+    printSeparator();
+    std::cout << "Mini-Rijndael Logarithm Tables Demonstration" << std::endl;
+    printSeparator();
+    uint16_t generator = 0x0002; // Example generator
+    uint16_t polynomial = 0x002B; // P16(X) = X16 + X5 + X3 + X2 + 1
+    auto logTable = generateLogTable(generator, polynomial);
+    auto inverseLogTable = generateInverseLogTable(logTable);
+    auto zechLogTable = generateZechLogTable(logTable, inverseLogTable);
+    // Write the tables to a file
+    std::ofstream outFile("logarithm_tables.txt");
+    if (outFile.is_open()) {
+        outFile << "Logarithm Table:" << std::endl;
+        for (size_t i = 0; i < logTable.size(); ++i) {
+            outFile << "log(" << i << ") = " << logTable[i] << std::endl;
+        }
+        outFile << std::endl << "Inverse Logarithm Table:" << std::endl;
+        for (size_t i = 0; i < inverseLogTable.size(); ++i) {
+            outFile << "antilog(" << i << ") = " << inverseLogTable[i] << std::endl;
+        }
+        outFile << std::endl << "Zech's Logarithm Table:" << std::endl;
+        for (size_t i = 0; i < zechLogTable.size(); ++i) {
+            outFile << "Zech(" << i << ") = " << zechLogTable[i] << std::endl;
+        }
+        outFile.close();
+    } else {
+        std::cerr << "Unable to open file for writing." << std::endl;
+    }
+    std::cout << "Logarithm tables have been written to 'logarithm_tables.txt'." << std::endl;
     printSeparator();
     std::cout << "End of Demonstration" << std::endl;
     printSeparator();
